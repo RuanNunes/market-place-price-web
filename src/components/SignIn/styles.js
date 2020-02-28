e7f0a0fbc2606bd6c7db88e0896246fdecb7435c
Gabriell Huver <gabriell.is.huver@gmail.com>
2020-02-28 14:13:45 -0300

    refatorando pages e components

--- src/components/SignIn/styles.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-import { Layout } from 'antd'
-export const Container = styled(Layout.Content)`
-    margin-top:120px;
-    min-width:260px;
-    width: 21vw;
-    height:100%;
-    align-self:center;
-`;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Routes from './components/Routes'
+import Routes from './Routes'
 import 'ant-design-pro/dist/ant-design-pro.css';
 import 'antd/dist/antd.css';
 ReactDOM.render(<Routes />, document.getElementById('root'));
